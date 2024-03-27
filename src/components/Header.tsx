46a4cef3ebe71cca240a4ccbd50a9bf1eb9182d8
Alexys Jordan <alexyskjordan@gmail.com>
2024-03-27 14:30:17 -0600

    Add first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+
+
+// export const Header = ({ handleSearch, calculateTotalQuantity }) => { 
+//     return (
+//         <div className="header-container">
+//             {/* <NavLink to="/"> */}
+//                 <button className="home-click">Home</button>
+//             {/* </NavLink> */}
+//             <h1>
+//                 {/* <Link to="/">Lexyeful Food Source</Link> */}
+//                 </h1>
+//             {/* <SearchBar handleSearch={handleSearch} />  */}
+//             {/* <NavLink to="/cart"> */}
+//                 <button className="cart-click">Cart <span>{calculateTotalQuantity || 0}</span></button>
+//             {/* </NavLink> */}
+//         </div>
+//     );
+// };
---
+++ src/pages/index.tsx
@@ -0,0 +1,43 @@
+/// index.tsx is the same as app in a react app
+
+import React, { useState } from 'react';
+import { fetchFoodData } from '@/data/apicalls';
+
+interface SearchResult {
+  id: number;
+  label: string;
+  image: string;
+}
+
+ const App: React.FC = () => {
+const [searchedResults, setSearchedResults] = useState<SearchResult[]>([])
+const [selectedItems, setSelectedItems] = useState<SearchResult[]>([])
+
+
+const handleSearch = (query: string): void => {
+  fetchFoodData(query)
+    .then((data: any) => {
+      if (data.hints) {
+        const newArray: SearchResult[] = data.hints.map((hint: any) => ({
+          id: hint.food.foodId,
+          label: hint.food.label,
+          image: hint.food.image,
+        }));
+        setSearchedResults(newArray);
+      } else {
+        setSearchedResults([]);
+      }
+    })
+    .catch((error: Error) => {
+      console.error('Error fetching data', error);
+      setSearchedResults([]);
+    });
+};
+return (
+  <div className="App">
+    <h1>Hi</h1>
+</div>
+);
+}
+
+export default App;
