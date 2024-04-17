cf4c756cf7bdb221e5522f05c1e5300b987ac3dd
Alexys Jordan <alexyskjordan@gmail.com>
2024-04-17 08:00:19 -0600

    Update food Item

--- src/components/FoodItem.tsx
+++ src/components/FoodItem.tsx
@@ -1,21 +1,36 @@
 // FoodItem.tsx
 import React from 'react';
 
-interface FoodItemProps {
-  item: {
-    id: number;
-    label: string;
-    image: string;
-  };
-  buttonDistinction: string;
-  index: number;
+interface FoodItemType {
+  id: number;
+  label: string;
+  image: string;
+  quantity: number;
 }
 
-export const FoodItem: React.FC<FoodItemProps> = ({ item, buttonDistinction, index }) => {
+interface FoodItemProps {
+  item: FoodItemType;
+  buttonDistinction: string;
+  index: number;
+  addToCart: (product: FoodItemType) => void;  // Use the type here
+}
+
+// interface FoodItemProps {
+//   item: {
+//     id: number;
+//     label: string;
+//     image: string;
+//     addToCart: (product: FoodItem) => void;
+//   };
+//   buttonDistinction: string;
+//   index: number;
+// }
+
+export const FoodItem: React.FC<FoodItemProps> = ({ item, buttonDistinction, index, addToCart }) => {
   return (
-    <div key={index} className="food-item-card">
+    <div className="food-item-card">
       <img src={item.image || ''} alt={item.label} className="food-item-image" />
-      <button  className="item-button">
+      <button onClick={() => addToCart(item)} className="item-button">
         {buttonDistinction}
       </button>
     </div>
